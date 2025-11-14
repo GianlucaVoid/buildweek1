6ca920f030f6ba38e32fc7fe8005c8efe7cebc6f
gianluca pirisi <pirisi.gianluca1997@gmail.com>
2025-11-14 19:48:23 +0100

    aggiunta ciclo per iterazione array

--- java.js
+++ java.js
@@ -98,6 +98,38 @@ const questions = [
   },
 ];
 
+//FUNZIONE PER SELEZIONARE I BUTTON E DARE L'EFFETTO SELEZIONE
+
+const allButton = document.querySelectorAll('.btn-answers')
+
+function clickAnswer(event) {
+
+  allButton.forEach(button => {
+    button.classList.remove('button-click')
+  });
+
+  event.currentTarget.classList.add('button-click')
+}
+
+allButton.forEach(button => {
+  button.addEventListener('click', clickAnswer)
+})
+
+//CICLO FOR PER ITERARE LE DOMANDE
+
+let domande = []
+let questionOnHtml = document.querySelector('#question')
+
+for (let i = 0; i < questions.length; i++) {
+  let domande = questions[i].question
+
+  domande = questions[Math.floor(Math.random() * questions.length)]
+
+  questionOnHtml.innerText = domande.question
+  console.log(domande)
+}
+
+
 let domandaCorrente = 0; // per cambiare domanda e vederla sul browser
 let punteggio = 0;
 
